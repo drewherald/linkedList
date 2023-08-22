bf568807c0e566e18273c64aab419016bfa27a63
drewherald <drewherald9@gmail.com>
2023-08-22 16:54:04 -0400

    finished linkedList exercise

--- LinkedList.js
+++ LinkedList.js
@@ -7,13 +7,111 @@ export default class LinkedList{
 
     append(value){
         if(this.listStart==null){
-            value == this.listStart
+            new Node(value) == this.listStart
         } else{
             let check = this.listStart
             while(check.nextNode!=null){
                 check = check.nextNode
             }
+            check.nextNode = new Node(value)
+        }
+    }
+
+    prepend(value){
+        if(this.listStart==null){
+            new Node(value) == this.listStart
+        }else{
+            temp = new Node(value)
+           temp.nextNode = this.listStart
+           this.listStart = temp
+        }
+    }
+
+    size(){
+        if(this.listStart==null){
+            return 0;
+        }
+
+        let check = this.listStart
+        let count = 0;
+        while(check.nextNode!=null){
+            check = check.nextNode
+            count++
+        }
+        return count;
+    }
+
+    head(){
+        return this.listStart;
+    }
+
+    tail(){
+        let check = this.listStart
+        while(check.nextNode!=null){
+            check = check.nextNode
+        }
+        return check;
+    }
+
+    at(index){
+        let check = this.listStart
+        for(let i=0; i < index; i++){
+            check = check.nextNode
+            if(check==null){
+                return("error, no node found at this index")
+            }
+        }
+        return check;
+    }
+
+    pop(){
+        let check = this.listStart
+        let remover = null
+        while(check.nextNode!=null){
+            check = check.nextNode
+            remover = check;
+        }
+
+        remover.nextNode = null;
+    }
+
+    contains(value){
+        let check = this.listStart
+        while(check.nextNode!=null){
+            if(check.value===value){
+                return true;
+            }
+            check = check.nextNode
+        }
+        return false;
+    }
+
+    find(value){
+        let check = this.listStart
+        let count = 0;
+        while(check.nextNode!=null){
+            if(check.value===value){
+                return count;
+            }
+            check = check.nextNode
+            count++
+        }
+
+        return "value not found!"
+    }
+
+    toString(){
+        if(this.listStart==null){
+            return "null"
+        } else{
+            let check = this.listStart
+            let print = ""
+            while(check.nextNode!=null){
+                print += `(${check.value}) -> `
+                check = check.nextNode
+            }
+            print += "null"
+            return print;
         }
     }
 }
-
